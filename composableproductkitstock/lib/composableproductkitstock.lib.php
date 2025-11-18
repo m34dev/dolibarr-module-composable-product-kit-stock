91c2b663fd5c410dfc235c1ffcfc145a1c211604
William Mead <william@m34d.com>
2025-11-18 15:16:36 +0100

    Added empty Dolibarr module

---
+++ composableproductkitstock/lib/composableproductkitstock.lib.php
@@ -0,0 +1,85 @@
+<?php
+/* Copyright (C) 2025       William Mead    <william@m34d.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    composableproductkitstock/lib/composableproductkitstock.lib.php
+ * \ingroup composableproductkitstock
+ * \brief   Library files with common functions for ComposableProductKitStock
+ */
+
+/**
+ * Prepare admin pages header
+ *
+ * @return array<array{string,string,string}>
+ */
+function composableproductkitstockAdminPrepareHead()
+{
+	global $langs, $conf;
+
+	// global $db;
+	// $extrafields = new ExtraFields($db);
+	// $extrafields->fetch_name_optionals_label('myobject');
+
+	$langs->load("composableproductkitstock@composableproductkitstock");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = dol_buildpath("/composableproductkitstock/admin/setup.php", 1);
+	$head[$h][1] = $langs->trans("Prerequisites");
+	$head[$h][2] = 'Checks';
+	$h++;
+
+	/*
+	$head[$h][0] = dol_buildpath("/composableproductkitstock/admin/myobject_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFields");
+	$nbExtrafields = (isset($extrafields->attributes['myobject']['label']) && is_countable($extrafields->attributes['myobject']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafields';
+	$h++;
+
+	$head[$h][0] = dol_buildpath("/composableproductkitstock/admin/myobjectline_extrafields.php", 1);
+	$head[$h][1] = $langs->trans("ExtraFieldsLines");
+	$nbExtrafields = (isset($extrafields->attributes['myobjectline']['label']) && is_countable($extrafields->attributes['myobjectline']['label'])) ? count($extrafields->attributes['myobject']['label']) : 0;
+	if ($nbExtrafields > 0) {
+		$head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbExtrafields . '</span>';
+	}
+	$head[$h][2] = 'myobject_extrafieldsline';
+	$h++;
+	*/
+
+	$head[$h][0] = dol_buildpath("/composableproductkitstock/admin/about.php", 1);
+	$head[$h][1] = $langs->trans("About");
+	$head[$h][2] = 'about';
+	$h++;
+
+	// Show more tabs from modules
+	// Entries must be declared in modules descriptor with line
+	//$this->tabs = array(
+	//	'entity:+tabname:Title:@composableproductkitstock:/composableproductkitstock/mypage.php?id=__ID__'
+	//); // to add new tab
+	//$this->tabs = array(
+	//	'entity:-tabname:Title:@composableproductkitstock:/composableproductkitstock/mypage.php?id=__ID__'
+	//); // to remove a tab
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'composableproductkitstock@composableproductkitstock');
+
+	complete_head_from_modules($conf, $langs, null, $head, $h, 'composableproductkitstock@composableproductkitstock', 'remove');
+
+	return $head;
+}
