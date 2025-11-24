d316aeae0391f1d855f888e20cfd480b58ad0fa6
William Mead <william@m34d.com>
2025-11-24 15:03:40 +0100

    Updated changelog. Updated version.

--- composableproductkitstock/core/modules/modComposableProductKitStock.class.php
+++ composableproductkitstock/core/modules/modComposableProductKitStock.class.php
@@ -73,7 +73,7 @@ class modComposableProductKitStock extends DolibarrModules
 		$this->editor_squarred_logo = 'logoSquareM34D.png@composableproductkitstock';					// Must be image filename into the module/img directory followed with @modulename. Example: 'myimage.png@composableproductkitstock'
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.1.0';
+		$this->version = '1.1.1';
 		// Url to the file with your last numberversion of this module
 		$this->url_last_version = 'https://lab.frogg.it/dolibarr/composableproductkitstock/-/raw/main/versionmodule.txt?ref_type=heads&inline=false';
 
