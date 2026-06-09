88e6c07ff853edc402d6de38c276423330da0122
William Mead <william@m34d.com>
2026-06-09 16:06:25 +0200

    Added extrafield column label translation

--- composableproductkitstock/export.php
+++ composableproductkitstock/export.php
@@ -64,7 +64,7 @@ $column_labels = array(
 if (!empty($extrafields->attributes['product']['label'])) {
 	foreach ($extrafields->attributes['product']['label'] as $key => $extralabel) {
 		$allowed_columns[] = 'extra_'.$key;
-		$column_labels['extra_'.$key] = $extralabel;
+		$column_labels['extra_'.$key] = $langs->trans($extralabel);
 	}
 }
 
